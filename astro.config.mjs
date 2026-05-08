d36da6d064c7d58e6119c1fcf8d1ef17a49bbbcc
kylebmoodley <kylebmoodley@gmail.com>
2026-05-08 19:32:31 +0200

    netlify patch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: 'https://yourdomain.co.za',
+  adapter: netlify(),
 });
